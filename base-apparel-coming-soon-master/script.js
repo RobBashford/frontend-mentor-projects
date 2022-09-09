0d7652055353d1ec05a791507d914257b8c83eeb
RobBashford <robertbashford@rocketmail.com>
2022-09-09 18:08:48 +0100

    Validation working

--- base-apparel-coming-soon-master/script.js
+++ base-apparel-coming-soon-master/script.js
@@ -1,2 +1,15 @@
 "use strict";
 
+function validate() {
+  const input = document.createElement("input");
+  input.type = "email";
+  input.value = document.getElementById("test").value;
+
+  if (input.checkValidity()) {
+    console.log("yes");
+  } else {
+    console.log("no");
+  }
+
+  return false;
+}
