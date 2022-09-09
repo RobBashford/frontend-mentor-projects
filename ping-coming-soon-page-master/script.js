857a92a528719251d6232f604005695f7bfd3384
RobBashford <robertbashford@rocketmail.com>
2022-09-09 19:40:50 +0100

    Finished Project
    javascript validation added

--- ping-coming-soon-page-master/script.js
+++ ping-coming-soon-page-master/script.js
@@ -1,18 +1,27 @@
 "use strict";
 
-const btn = document.querySelector(".btn");
-const email = document.querySelector(".email-input");
+const errorIcon = document.querySelector(".error-icon");
+const errorMsg = document.querySelector(".error-msg");
+const emailInput = document.querySelector(".email-input");
+const emailForm = document.querySelector(".email-form");
 
-function val() {
-  const form = document.getElementById("form");
-  const email = document.getElementById("email").value;
-  const text = document.getElementById("text");
+function validate() {
+  const input = document.createElement("input");
+  input.type = "email";
+  input.value = document.getElementById("test").value;
 
-  console.log(email);
+  // const html = `<p class="error-msg">Please provide a valid email address</p>`;
+
+  if (input.checkValidity()) {
+    console.log("yes");
+    emailInput.style.borderColor = "#4f7df3";
+    errorMsg.classList.add("hidden");
+  } else {
+    console.log("no");
+    emailInput.style.borderColor = "hsl(354, 100%, 66%)";
+    errorMsg.classList.remove("hidden");
+
+    // emailForm.insertAdjacentHTML("afterend", html);
+  }
+  return false;
 }
-
-const validation = function () {
-  console.log(email.textContent);
-};
-
-btn.addEventListener("click", validation);
